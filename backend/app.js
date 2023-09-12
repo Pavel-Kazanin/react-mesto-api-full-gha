0827f4962591eccf787877b2fba7097436a4ed21
Pavel Kazanin <pkazanin@yandex.ru>
2023-09-13 00:26:49 +0300

    Mesto. настройка и деплой бэкенда

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Credentials', true);
+    res.header('Credentials', 'true');
   }
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
