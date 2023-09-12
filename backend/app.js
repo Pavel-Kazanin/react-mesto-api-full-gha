8b5577bfddd63df4167e8661281dc66e4fbd4c1d
Pavel Kazanin <pkazanin@yandex.ru>
2023-09-13 00:34:16 +0300

    Mesto. настройка и деплой бэкенда

--- backend/app.js
+++ backend/app.js
@@ -37,10 +37,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
   }
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', DEFAULT_ALLOWED_METHODS);
     return res.end();
   }
 
